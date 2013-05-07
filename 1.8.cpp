a5beaf7f14b9e7a678d8d116dbdb979c41c50168
yuzujin@126.com <yuzujin@126.com>
2013-05-08 00:06:55 +0800

    1.8 commit

---
+++ 1.8.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+bool isSubString(char* str1, char* str2) {
+	if(strstr(str1, str2))
+		return true;
+	else
+		return false;
+}
+
+bool isRotation(string str1, string str2) {
+	if(str1.length() != str2.length()) return false;
+	string temp = str1 + str1;
+	if(isSubString(&temp[0], &str2[0]))
+		return true;
+	else
+		return false;
+}
+
+int main() {
+	string str1, str2;
+	cin >> str1;
+	cin >> str2;
+
+	if(isRotation(str1, str2))
+		cout << str2 << " is the rotation of " << str1 << endl;
+	else
+		cout << str2 << " is not the rotation of " << str1 << endl;
+	system("pause");
+	return 0;
+}
